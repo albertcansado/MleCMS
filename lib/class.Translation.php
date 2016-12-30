b4c46f6e693e3528d7ea0092b42cab56707bb8b0
Albert Cansado Solà <albert@MacBook-Pro-de-Albert.local>
2016-12-30 17:33:21 +0100

    CS

--- lib/class.Translation.php
+++ lib/class.Translation.php
@@ -13,42 +13,44 @@ class Translation {
     protected static $_translations = null;
     protected static $_mod = null;
 
-    /** static class */
-    private function __construct() {
-
-    }
-
-    private static function _init() {
-        if (self::$_mod == null)
+    private static function _init()
+    {
+        if (self::$_mod === null)
             self::$_mod = cms_utils::get_module('MleCMS');
 
-        if (self::$_lang == null)
+        if (self::$_lang === null)
             self::$_lang = CmsNlsOperations::get_current_language();
 
-        if (self::$_langs == null)
+        if (self::$_langs === null)
             self::$_langs = mle_tools::get_langs();
 
-        if (self::$_translations == null)
+        if (self::$_translations === null) {
             self::$_translations = json_decode(self::$_mod->GetPreference('translations'), true);
+        }
     }
 
-    public static function save() {
-        if(self::$_mod != null)
-        self::$_mod->SetPreference('translations', json_encode(self::$_translations));
+    public static function save()
+    {
+        if (self::$_mod !== null) {
+            self::$_mod->SetPreference('translations', json_encode(self::$_translations));
+        }
     }
 
-    public static function get_translations() {
+    public static function get_translations()
+    {
         self::_init();
         return self::$_translations;
     }
 
-    public static function remove($key) {
+    public static function remove($key)
+    {
         self::_init();
         unset(self::$_translations[$key]);
         self::save();
     }
 
-    public static function update($post) {
+    public static function update($post)
+    {
         self::_init();
         $editLang = $post['editLang'];
         $key = $post['editKey'];
@@ -56,12 +58,15 @@ class Translation {
         self::$_translations[$key][$editLang] = $value;
         self::save();
     }
-    public static function add_to_translations($key, $locale, $value) {
+
+    public static function add_to_translations($key, $locale, $value)
+    {
         self::_init();
         self::$_translations[$key][$locale] = $value;
     }
-    public static function translate($params) {
 
+    public static function translate($params)
+    {
         self::_init();
 
         $smarty = cmsms()->GetSmarty();
@@ -98,7 +103,6 @@ class Translation {
             echo $lang_value;
         }
     }
-
 }
 
 ?>
